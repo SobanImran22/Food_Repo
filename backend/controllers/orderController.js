0ee49a4e7e4d31b98f0f82785752a62691c91390
SobanImran22 <166215065+SobanImran22@users.noreply.github.com>
2025-10-22 22:33:58 +0500

    url changed

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -6,7 +6,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 // ✅ Place order
 const placeOrder = async (req,res)=>{
-    const frontend_url= "https://lahori-kababish-restaurent.netlify.app"; 
+    const frontend_url= "https://www.lahorikebabish.co.uk/"; 
     
     try {
         // Line items bana rahe hain stripe checkout ke liye
